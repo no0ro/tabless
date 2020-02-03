8bc310382afd11f3001a9b2e181a0b0ea8a0ff85
Noelle Rolwes <nrolwes@gmail.com>
2020-02-03 00:36:07 -0600

    added seed data

---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+Tab.create(:name => "openSSL", :url => "https://github.com/dotnet/cli/issues/5518", :notes => "github cant install openSSL via homebrew" )
+Tab.create(:name => "AR migrations", :url => "https://edgeguides.rubyonrails.org/active_record_migrations.html", :notes => "rails doc, activerecord migrations more info" )
+Tab.create(:name => "git init", :url => "https://www.atlassian.com/git/tutorials/setting-up-a-repository/git-init", :notes => "more details about git init" )
