136cb5ed62de17298bbdcadff9408e31382a9b8e
Noelle Rolwes <nrolwes@gmail.com>
2020-02-04 23:12:20 -0600

    added #new, #create, #index routes

--- app/controllers/tabs_controller.rb
+++ app/controllers/tabs_controller.rb
@@ -2,29 +2,33 @@ class TabsController <  ApplicationController
 
   # all tabs
   get '/tabs' do
-    binding.pry
-
-    #if logged_in?
-      # ^^ NOT logged in so cant uncomment
-      @tabs = Tab.all
-
+    #binding.pry
+    if logged_in?
+      @user = current_user
+      @tabs = Tab.where("user_id" => @user.id)
       # puts session[:user_id] #test
       # puts "above is inside tabs session[:user_id]" #test
-      puts current_user
-
-      #binding.pry
       erb :'tabs/index'
-
-    #else
-      #redirect to '/signup'
-    #end
+    else
+      redirect to '/signup'
+    end
   end
 
+  #new
   get '/tabs/new' do
     @tab = Tab.new
     erb :'/tabs/new'
   end
 
+  #create
+  post '/tabs' do
+    user = current_user
+    tab = Tab.create(:name => params["tab_name"], :url => params["url"], :notes => params["notes"])
+    
+
+  end
+
+  #show
   post '/tabs/:id' do
 
   end
