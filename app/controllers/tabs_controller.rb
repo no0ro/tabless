eabae7d4ef6042c2bf68ed3b3efadbb5505cf3af
Noelle Rolwes <nrolwes@gmail.com>
2020-02-05 23:03:52 -0600

    added routes and views for  edit, index, show

--- app/controllers/tabs_controller.rb
+++ app/controllers/tabs_controller.rb
@@ -5,32 +5,70 @@ class TabsController <  ApplicationController
     #binding.pry
     if logged_in?
       @user = current_user
-      @tabs = Tab.where("user_id" => @user.id)
+      @tabs = current_user.tabs
+      puts @tabs #["user_id", 38]
+      puts "^@tabs"
+      puts @user ##<User:0x00007fe5783543b8>
+      puts "^@user"
+      puts current_user #<User:0x00007fe57a01dc60>
       # puts session[:user_id] #test
       # puts "above is inside tabs session[:user_id]" #test
+      #binding.pry
       erb :'tabs/index'
     else
-      redirect to '/signup'
+      redirect to '/'
     end
   end
 
-  #new
+  # new - user can create a new tab  IF logged in
   get '/tabs/new' do
+    #if logged_in?
     @tab = Tab.new
     erb :'/tabs/new'
   end
 
   #create
   post '/tabs' do
-    user = current_user
-    tab = Tab.create(:name => params["tab_name"], :url => params["url"], :notes => params["notes"])
-    
-
+    #binding.pry
+    tab = Tab.new(:name => params["tab_name"], :url => params["url"], :notes => params["notes"])
+    tab.user_id = current_user.id
+    tab.save
+    #binding.pry
+    redirect_to_homepage
   end
 
   #show
-  post '/tabs/:id' do
-
+  get '/tabs/:id' do
+    if logged_in?
+        #binding.pry
+        @tab = Tab.find_by_id(params[:id])
+        if @tab.user == current_user #make sure pulling up only this user's saved tab. not someone elses saved tab
+          erb :'tabs/show'
+        else
+          redirect to '/login'
+        end
+    else
+      redirect_to_homepage
+    end
   end
+
+  #edit
+  get '/tabs/:id/edit' do
+    if logged_in?
+      @tab = Tab.find_by_id(params[:id])
+      if @tab.user == current_user #make sure pulling up only this user's saved tab. not someone elses saved tab
+        erb :'tabs/edit'
+      else
+        redirect to '/login'
+      end
+    else
+      redirect_to_homepage
+    end
+  end
+
+  #update
+
+  #delete
+
 
 end
