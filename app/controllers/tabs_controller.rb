112290d2d1b504ddae6423890e6e3c5a26ace1ca
Noelle Rolwes <nrolwes@gmail.com>
2020-02-11 15:47:24 -0600

    cleaned  up comments

--- app/controllers/tabs_controller.rb
+++ app/controllers/tabs_controller.rb
@@ -5,38 +5,39 @@ class TabsController <  ApplicationController
     if logged_in?
       @user = current_user
       @tabs = current_user.tabs
-      # puts @tabs #["user_id", 38]
-      # puts "^@tabs"
-      # puts @user ##<User:0x00007fe5783543b8>
-      # puts "^@user"
-      # puts current_user #<User:0x00007fe57a01dc60>
-      # puts session[:user_id] #test
-      # puts "above is inside tabs session[:user_id]" #test
-      #binding.pry
       erb :'tabs/index'
     else
       redirect to '/'
     end
   end
 
-  # new - user can create a new tab  IF logged in
+  # new - renders new tab form IF user is logged in
   get '/tabs/new' do
-    #if logged_in?
-    @tab = Tab.new
-    erb :'/tabs/new'
+    if logged_in?
+      erb :'/tabs/new'
+    else
+      redirect to '/'
+    end
   end
 
-  #create
+  # create
   post '/tabs' do
-    #binding.pry
-    tab = Tab.new(:name => params["tab_name"], :url => params["url"], :notes => params["notes"])
-    tab.user_id = current_user.id
-    tab.save
-    #binding.pry
-    redirect_to_homepage
+    if logged_in?
+      # 1) take user input & create a new tab
+      tab = Tab.new(:name => params["tab_name"], :url => params["url"], :notes => params["notes"])
+
+      # 2) assign the user's id #, to the tabs user_id key
+      # aka associate the tab to the user
+      tab.user_id = current_user.id
+
+      tab.save
+      redirect_to_homepage
+    else
+      redirect to '/'
+    end
   end
 
-  #show
+  # show
   get '/tabs/:id' do
     if logged_in?
         @tab = Tab.find_by_id(params[:id])
@@ -55,7 +56,7 @@ class TabsController <  ApplicationController
   get '/tabs/:id/edit' do
     if logged_in?
       @tab = Tab.find_by_id(params[:id])
-      if @tab.user == current_user #make sure pulling up only this user's saved tab. not someone elses saved tab
+      if @tab.user == current_user # make sure pulling up only this user's saved tab. not someone elses saved tab
         erb :'tabs/edit'
       else
         redirect to '/login'
